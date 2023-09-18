8664edb0ef96f677de8f371a40ef27184adb6f4a
wooriview0 <131226720+wooriview0@users.noreply.github.com>
2023-09-18 15:47:19 +0900

    재고,수입차, 국산차, 재고, 제조사, 채팅프로필 추가

--- js/common.js
+++ js/common.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){
+    // 채팅 메시지 애니메이션
 	$(".m-message").each(function (index, item){
         setTimeout(function(){
             $(item).addClass("active");
         }, 300 * index)
+    });
+
+    // 검색필터
+    $(".m-filter.type01 .box-title").click(function(){
+       $(this).siblings(".box-content").slideToggle();
+       $(this).toggleClass("active");
+    });
+    
+    // 채팅 프로필 팝업
+    $(".m-messages.type01 > .m-message > .img-wrap").click(function(){
+       $(".m-profile.type01").addClass("active");
+    });
+
+    $(".m-profile.type01 .black").click(function(){
+        $(".m-profile.type01").removeClass("active");
     });
 });
